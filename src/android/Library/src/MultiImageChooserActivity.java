0631e9f6dac7c250602a1b9e06d583aac650341d
Nikita <pashkov.nikita@yahoo.com>
2017-10-03 19:09:07 +0300

    Russian translation (retarded)

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -173,8 +173,8 @@ public class MultiImageChooserActivity extends Activity implements OnItemClickLi
         setupHeader();
         updateAcceptButton();
         progress = new ProgressDialog(this);
-        progress.setTitle("Processing Images");
-        progress.setMessage("This may take a few moments");
+        progress.setTitle("Обработка изображений");
+        progress.setMessage("Это может занять некоторое время");
     }
     
     @Override
